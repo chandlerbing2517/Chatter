c524377a92fd19fd93da1a4da007c4e7e1e3e805
sajal <Jainsajal519@gmail.com>
2024-03-18 23:55:09 +0530

    Free commit

--- client/src/components/layout/appLayout.jsx
+++ client/src/components/layout/appLayout.jsx
@@ -9,9 +9,10 @@ const appLayout = () => (WrappedComponent)=> {
       <Title />
       <Header />
       <Grid container height={"calc(100vh-4rem)"}>
-        <Grid item xs={4} height={"100%"}>First</Grid>
-        <Grid item xs={4} height={"100%"}><WrappedComponent {...props} /></Grid>
-        <Grid item xs={4} height={"100%"}>Third</Grid>
+        <Grid item sm={4} md ={3} sx={{display:{xs:"none", sm:"block"},}} height={"100%"}>First</Grid>
+        <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"} ><WrappedComponent {...props} /></Grid>
+        <Grid item md={4} lg={3} height={"100%"} sx={{display:{xs:"none",md:"block"},padding:"2rem",bgcolor:"rgb(0,0,0,0.85)"
+        }}>Third</Grid>
       </Grid>
     </>
   )
