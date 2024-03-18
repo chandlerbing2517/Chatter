1e80f107672ac6ad339f5e437e00f09a0897d7e5
sajal <Jainsajal519@gmail.com>
2024-03-19 02:05:05 +0530

    Added ToolBar

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,11 +1,62 @@
+import { AppBar, Toolbar, Box, Typography, IconButton, Tooltip } from '@mui/material'
 import React from 'react'
-
+import { orange } from '../../constants/color'
+import {Menu as MenuIcon, Search as SearchIcon, Add as AddIcon} from '@mui/icons-material';
+const handleMobile=()=>
+{
+  console.log('Handle Mobile');
+}
+const openSearch=()=>
+{
+  console.log('Open Search');
+}
+const openNewGroup=()=>
+{
+  console.log('Open NewGroupe');
+}
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <>
+    <Box sx={{flexGrow: 1}} height={"4rem"}>
+      <AppBar
+      position='static'
+      sx={{
+      bgcolor: orange
+      }}>
+        <Toolbar>
+          <Typography variant='h6'
+          sx={{
+            display:{xs:'none', sm:'block'},
+          }}>
+            Chatter
+          </Typography>
+          <Box           
+          sx={{
+            display:{xs:'block', sm:'none'},
+          }}>
+            <IconButton color='inherit' onClick={handleMobile}>
+              <MenuIcon />
+            </IconButton>
+          </Box>
+          <Box sx={{flexGrow:1}} />
+          <Box>
+            <Tooltip>
+            <IconButton color='inherit' onClick={openSearch}>
+              <SearchIcon />
+            </IconButton>
+            </Tooltip>
+            <Tooltip name="New Group">
+            <IconButton color='inherit' onClick={openNewGroup}>
+              <AddIcon />
+            </IconButton>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Box>
+    </>
+  );
+  
+};
 
 export default Header
--- client/src/components/layout/appLayout.jsx
+++ client/src/components/layout/appLayout.jsx
@@ -8,7 +8,7 @@ const appLayout = () => (WrappedComponent)=> {
     <>
       <Title />
       <Header />
-      <Grid container height={"calc(100vh-4rem)"}>
+      <Grid container height={"calc(100vh - 4rem)"}>
         <Grid item sm={4} md ={3} sx={{display:{xs:"none", sm:"block"},}} height={"100%"}>First</Grid>
         <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"} ><WrappedComponent {...props} /></Grid>
         <Grid item md={4} lg={3} height={"100%"} sx={{display:{xs:"none",md:"block"},padding:"2rem",bgcolor:"rgb(0,0,0,0.85)"
